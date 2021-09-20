c1fc9f0adaf11a2a5cacac7b0fbf6e697c052a0a
Gabriel Oliveira <g4brieloliveira.dev@gmail.com>
2021-09-20 02:27:41 -0300

    adding firebase

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { RestaurantsContextProvider } from "./src/services/restaurantServices/restaurants-context";
 import { LocationContextProvider } from "./src/services/location/location-context";
 import { FavouritesContextProvider } from "./src/services/favourites/favourites-context";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastucture/theme";
 import { Navigation } from "./src/infrastucture/navigation";
+import firebase from "firebase/app";
+require("firebase/auth");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDool9gZNOSmyEOTVg_GHRlMK-bGpJjDpI",
+  authDomain: "mealstogo-5fcb8.firebaseapp.com",
+  projectId: "mealstogo-5fcb8",
+  storageBucket: "mealstogo-5fcb8.appspot.com",
+  messagingSenderId: "746766000569",
+  appId: "1:746766000569:web:14c202a638a5de1c5ed668",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 import {
   useFonts as useInter,
@@ -30,19 +45,34 @@ import {
 } from "@expo-google-fonts/oswald";
 
 export default function App() {
-  const [interLoaded] = useInter({ Inter_400Regular });
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword("email@gmail.com", "password")
+        .then((user) => {
+          setIsAuthenticated(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }, 2000);
+  }, []);
 
+  const [interLoaded] = useInter({ Inter_400Regular });
+  const [rajdhaniLoaded] = useRajdhani({ Rajdhani_400Regular });
+  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
   const [poppinsLoaded] = usePoppins({
     Poppins_400Regular,
     Poppins_700Bold,
     Poppins_600SemiBold,
   });
-
-  const [rajdhaniLoaded] = useRajdhani({ Rajdhani_400Regular });
-
-  const [oswaldLoaded] = useOswald({ Oswald_400Regular });
-
   if (!interLoaded || !poppinsLoaded || !rajdhaniLoaded || !oswaldLoaded) {
+    return null;
+  }
+
+  if (!isAuthenticated) {
     return null;
   }
 
